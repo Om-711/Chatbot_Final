74c5ed5374f19940fcb6c4857bbd6603d092ac97
Om_711 <154738879+Om-711@users.noreply.github.com>
2025-10-18 15:14:43 +0530

    Update check.py

--- check.py
+++ check.py
@@ -2,7 +2,8 @@ from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 from pymongo import MongoClient
-from langchain.text_splitters import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
 from langchain_community.vectorstores import FAISS
 # from langchain_community.embeddings import SentenceTransformerEmbeddings
 from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAIEmbeddings
@@ -142,5 +143,6 @@ async def chat(user_id: str, option: str):
         return await chat(user_id, "main")
 
     return JSONResponse({"message": "Invalid option.", "options": ["Back"]})
+
 
 
